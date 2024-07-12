f50009da42b323777acb5e7ab3ccd719cb21f004
Anathalia Izidro <49588396+AnathaliaIzidro@users.noreply.github.com>
2024-07-12 12:12:54 -0300

    Add files via upload

---
+++ distancia.py
@@ -0,0 +1,13 @@
+# 5. Crie uma variável para armazenar a distancia percorrida de um veiculo e em seguida exiba o consumo do veiculo nessa distancia, considerando que o veiculo faz 10km/L
+
+# distancia = 230
+# consumoVeiculo = 10
+# consumoViagem = distancia / consumoVeiculo
+# print(f"Você gastou {consumoViagem} litros")
+
+distancia = 1050
+rendimento = 10
+
+consumo = distancia / rendimento
+
+print(f'O consumo em litros de gasolina foi : {consumo:.2f} litros')
